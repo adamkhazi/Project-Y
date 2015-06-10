ddc505a2ae2349bd943adb1c9c58c20c813791b7
adamkhazi <adamk4@gmail.com>
2015-06-10 12:10:44 +0700

    Added App icons and pay and receive button icons

---
+++ Project Y/ReceiveFormPayersTableTVC.swift
@@ -0,0 +1,9 @@
+//
+//  ReceiveFormPayersTableTVC.swift
+//  Project Y
+//
+//  Created by Adam Khazi on 09/06/2015.
+//  Copyright (c) 2015 Adam Khazi. All rights reserved.
+//
+
+import Foundation
